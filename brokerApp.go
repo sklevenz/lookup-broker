92674d280181d1b5eea1156e0f4aa0d9aa3297cc
Stephan Klevenz <stephan@klaeff.de>
2020-12-21 12:09:43 +0100

    add application

---
+++ brokerApp.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"os"
+
+	"github.com/gorilla/mux"
+)
+
+const (
+	defaultPort = "5000"
+)
+
+var (
+	// Version set by go build via -ldflags "'-X main.Version=1.0'"
+	Version string = "n/a"
+	// Commit set by go build via -ldflags "'-X main.Commit=123'"
+	Commit string = "n/a"
+)
+
+func main() {
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = defaultPort
+	}
+
+	log.Printf("start application on port: %v", port)
+	log.Printf("version: %v", Version)
+	log.Printf("commit: %v", Commit)
+
+	brokerServer := mux.NewRouter().StrictSlash(true)
+
+	log.Printf("call server: http://localhost:%v", port)
+
+	if err := http.ListenAndServe(":"+port, brokerServer); err != nil {
+		log.Fatalf("could not listen on port %v: %v", port, err)
+	}
+}
