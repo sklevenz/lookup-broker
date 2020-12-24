c856ba570a58b6e1e620366b7d094ff7fb65995a
Stephan Klevenz <stephan.klevenz@sap.com>
2020-12-24 15:25:28 +0100

    maintain make

--- README.md
+++ README.md
@@ -14,18 +14,25 @@ usage: make.py [-h] [-v] [{build,run,test,generate,release,login,push}]
 | build | clean go build |
 | run | run server as localhost |
 | test | go test ./... |
-| generate| generate OSBAPI api files |
+| generate | generate OSBAPI api files |
+| config | generate an application manifest and a login config file |
 | release | release a new version |
 | login | login to cloud foundry |
 | push | push code to cloud foundry |
 
 ## local make configuration
 
+call
+
 ````
+
+./bin/make.py config
+
 .
 ├── LICENSE
-├── local
-│   └── cloud-foundry.yml
+├── gen
+│   └── config.yml
+│   └── manifest.yml
 
 cloud-foundry:
   api-url:  https://api.cf.eu10.hana.ondemand.com
@@ -34,4 +41,28 @@ cloud-foundry:
     password: password
     org: org name
     space: space name
+````
+## brew dependencies
+
+````
+brew list
+coreutils               jq                      bash                    curl                    go                      goreleaser
+python@3.9              cf-cli                  cf-cli@7                openapi-generator
+````
+
+## python dependencies
+
+````
+pip3 list
+Package     Version
+----------- -------
+autopep8    1.5.4
+pip         20.3.3
+protobuf    3.14.0
+pycodestyle 2.6.0
+PyYAML      5.3.1
+setuptools  51.0.0
+six         1.15.0
+toml        0.10.2
+wheel       0.36.1
 ````
--- bin/make.py
+++ bin/make.py
@@ -1,106 +1,162 @@
 #!/usr/bin/env python3
 
-import os
 import argparse
+import os
+from pathlib import Path
+from shutil import copyfile
 import yaml
 
+
 def test(verbose):
-    print ("-- vet & test broker")
+    print("-- vet & test broker")
     os.system("go vet ./...")
     if verbose:
-      os.system("go test ./... -v")
-    else:    
-      os.system("go test ./...")
+        os.system("go test ./... -v")
+    else:
+        os.system("go test ./...")
+
 
 def build(verbose):
-    print ("-- clean broker")
+    print("-- clean broker")
     os.system("go clean -r -cache -testcache -modcache")
-    print ("-- fmt broker")
+    print("-- fmt broker")
     os.system("go fmt ./...")
     print("-- build broker")
     flg = ldflags()
     os.system("go build " + flg + "-v ./...")
     if verbose:
-      print (flg)
+        print(flg)
+
 
 def run(verbose):
-    print ("-- run broker")
+    print("-- run broker")
     os.system("go run  " + ldflags() + " brokerApp.go")
 
+
 def generate(verbose):
-    print ("-- generate broker")
+    print("-- generate broker")
+
+    Path("./gen").mkdir(exist_ok=True)
 
     os.system("rm -rf ./gen ./openapi")
     os.system("mkdir -p ./gen ./openapi")
     os.system("wget https://raw.githubusercontent.com/openservicebrokerapi/servicebroker/master/swagger.yaml -O \"./gen/swagger.yaml\"")
     os.system("openapi-generator validate -i ./gen/swagger.yaml")
-    os.system("openapi-generator generate -i ./gen/swagger.yaml -g go-server -o ./gen")
+    os.system(
+        "openapi-generator generate -i ./gen/swagger.yaml -g go-server -o ./gen")
     os.system("cp ./gen/go/model_* ./openapi")
     os.system("go fmt ./openapi")
 
+
 def release(verbose):
-    print ("-- release broker")
-    print ("-- tbd")
-    print ("-- verbose:", verbose)
+    print("-- release broker")
+    print("-- tbd")
+    print("-- verbose:", verbose)
+
 
 def login(verbose):
-    print ("-- login cloud foundry")
+    print("-- login cloud foundry")
 
-    with open("./local/cloud-foundry.yml", 'r') as stream:
+    config = Path("./gen/config.yml")
+    if not config.exists():
+        print("call \'./bin/make.py config\'")
+        exit()
+
+    with open(config, 'r') as stream:
         try:
             cfConfig = yaml.safe_load(stream)
         except yaml.YAMLError as exc:
             print(exc)
-            os._exit(0)
+            exit()
 
     os.system("cf api {}".format(cfConfig.get("cloud-foundry").get("api-url")))
-    os.system("cf auth {} {}".format(cfConfig.get("cloud-foundry").get("login").get("user"), cfConfig.get("cloud-foundry").get("login").get("password")))
-    os.system("cf target -o {} -s {}".format(cfConfig.get("cloud-foundry").get("login").get("org"), cfConfig.get("cloud-foundry").get("login").get("space")))
+    os.system("cf auth {} {}".format(cfConfig.get("cloud-foundry").get("login").get("user"),
+                                     cfConfig.get("cloud-foundry").get("login").get("password")))
+    os.system("cf target -o {} -s {}".format(cfConfig.get("cloud-foundry").get(
+        "login").get("org"), cfConfig.get("cloud-foundry").get("login").get("space")))
+
 
 def push(verbose):
-    print ("-- push to cloud foundry")
+    print("-- push to cloud foundry")
 
-    os.system("cf push lookup-broker")
+    manifest = Path("./gen/manifest.yml")
+    if not manifest.exists():
+        print("call \'./bin/make.py config\'")
+        exit()
+
+    os.system("cf push -f {}".format(manifest))
+
+
+def config(verbose):
+    print("-- create configuration")
+
+    Path("./gen").mkdir(exist_ok=True)
+
+    manifest = Path("./gen/manifest.yml")
+    if manifest.exists():
+        print("{} file exists already".format(manifest))
+    else:
+        copyfile("./template/manifest-template.yml", manifest)
+        print("file created: {}".format(manifest))
+
+    config = Path("./gen/config.yml")
+    if config.exists():
+        print("{} file exists already".format(config))
+    else:
+        copyfile("./template/config-template.yml", config)
+        print("file created: {}".format(config))
+
 
 def dispatcher(cmd):
-   dispatcher={
-       'build': build,
-       "run": run,
-       "test": test,
-       "generate": generate,
-       "release": release,
-       "login": login,
-       "push": push,
-   }
-   return dispatcher.get(cmd)
- 
+    dispatcher = {
+        'build': build,
+        "run": run,
+        "test": test,
+        "generate": generate,
+        "config": config,
+        "release": release,
+        "login": login,
+        "push": push,
+    }
+    return dispatcher.get(cmd)
+
+
+def checkConfig():
+    if not os.path.isfile("./gen/manifest.yml") or not os.path.isfile("./gen/config.yml"):
+        print("call \'./bin/make.py config\'")
+        exit()
+
+
 def ldflags():
     dirtyStream = os.popen('git diff --quiet || echo dirty')
     dirty = dirtyStream.read()
     if dirty == "":
-      commitStream = os.popen('git rev-parse HEAD')
-      commit = commitStream.read()
+        commitStream = os.popen('git rev-parse HEAD')
+        commit = commitStream.read()
     else:
-      commit = dirty.strip()
+        commit = dirty.strip()
 
+    # TDOD: version (consider goreleaser)
+    return F"-ldflags=\"-X 'main.Version=0.0.0-snapshot' -X 'main.Commit={commit}'\""
 
-    # TDOD: version (consider goreleaser) 
-    return F"-ldflags=\"-X 'main.Version=dev' -X 'main.Commit={commit}'\""
 
 def main():
-  parser = argparse.ArgumentParser(description="Make tool for cloud foundry service lookup broker", epilog="(c) 2020 by KLÄFF-Soft)")
-  parser.add_help=True
-  parser.add_argument("command", nargs='?', choices=['build', 'run', 'test', 'generate', 'release', 'login', 'push'], help="commands to execute")
-  parser.add_argument("-v", action="store_true", help="verbose output")
+    parser = argparse.ArgumentParser(
+        description="Make tool for cloud foundry service lookup broker", epilog="(c) 2020 by KLÄFF-Soft)")
+    parser.add_help = True
+    parser.add_argument("command", nargs='?', choices=[
+                        'build', 'run', 'test', 'generate', 'config', 'release', 'login', 'push'], help="commands to execute")
+    parser.add_argument("-v", action="store_true", help="verbose output")
 
-  args = parser.parse_args()
+    args = parser.parse_args()
 
-  func = dispatcher(args.command)
+    func = dispatcher(args.command)
 
-  if func != None:
-    func(args.v)
-  else: 
-    parser.print_usage()
+    if func != None:
+        func(args.v)
+    else:
+        parser.print_usage()
+
 
 if __name__ == '__main__':
     main()
